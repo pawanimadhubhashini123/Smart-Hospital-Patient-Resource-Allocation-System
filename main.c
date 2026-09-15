4861702ad8ed26f77adcee4c29d0b0de90447ecf
pawanimadhubhashini123 <pawanimadubashini81@gmail.com>
2026-09-15 22:06:34 +0530

    add patient data arrays and global variables

--- main.c
+++ main.c
@@ -20,6 +20,29 @@ const double wardDailyRate[WARDS] = {3000.00,6000.00,12000.00,25000.00};
 
 const int wardCapacity[WARDS] = {20,10,10,5};
 
+int bedOccupancy[WARDS][MAX_BEDS] = {0};
+
+char patientID[MAX_PATIENTS][15];
+char patientName[MAX_PATIENTS][50];
+int patientAge[MAX_PATIENTS];
+int urgencyLevel[MAX_PATIENTS];
+int selectedSpecialty[MAX_PATIENTS];
+int admitted[MAX_PATIENTS];
+int selectedWard[MAX_PATIENTS];
+int daysAdmitted[MAX_PATIENTS];
+int assignedBed[MAX_PATIENTS];
+
+double baseFee[MAX_PATIENTS];
+double surcharge[MAX_PATIENTS];
+double wardCost[MAX_PATIENTS];
+double grossTotal[MAX_PATIENTS];
+double discount[MAX_PATIENTS];
+double finalAmount[MAX_PATIENTS];
+double waitingTime[MAX_PATIENTS];
+
+int patientCount = 0;
+int specialtyQueue[SPECIALTIES] = {0, 0, 0, 0};
+
 int main(void)
 {
     return 0;
